3c8ef3f76b8cc72d36caab3f2d12a4030ddfab3c
bogdanovicb <b.bogdanovic@case-3d.com>
2022-01-26 19:43:57 +0100

    feature: cart button animation on cart update.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,12 +4,28 @@ import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 class HeaderCartButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { btnIsHighlighted: false };
+  }
+  // bump the button on each prop update === useEffect(()=>{},[somePropChange])
+  componentDidUpdate(prevProps) {
+    if (prevProps.totalAmount !== this.props.totalAmount) {
+      this.setState({ btnIsHighlighted: true });
+      setTimeout(() => {
+        this.setState({ btnIsHighlighted: false });
+      }, 300);
+    }
+  }
   render() {
     const numberOfCartItems = this.props.items.reduce((curNumber, item) => {
       return curNumber + item.amount;
     }, 0);
+    const btnClasses = `${styles.button} ${
+      this.state.btnIsHighlighted ? styles.bump : ""
+    }`;
     return (
-      <button className={styles.button} onClick={this.props.onClick}>
+      <button className={btnClasses} onClick={this.props.onClick}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
