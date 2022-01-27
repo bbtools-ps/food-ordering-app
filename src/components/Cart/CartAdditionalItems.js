3bc7b708f35a0d08883d034909e6b600d3b7c376
Bogdan Bogdanovic <bogdi.mail@gmail.com>
2022-01-27 16:27:00 +0100

    bug when adding aditional items to store.

--- src/components/Cart/CartAdditionalItems.js
+++ src/components/Cart/CartAdditionalItems.js
@@ -1,9 +1,24 @@
+import { connect } from "react-redux";
 import { Component } from "react";
+import { cartActions } from "../../store";
 import styles from "./CartAdditionalItems.module.css";
 
 class CartItemAdditional extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { itemIsHighlighted: false };
+  }
   addItemHandler(item) {
+    this.setState((prevState) => {
+      return { itemIsHighlighted: !prevState.itemIsHighlighted };
+    });
     console.log(item);
+    // this.props.addItem({
+    //   id: item.id,
+    //   name: item.name,
+    //   amount: 1,
+    //   price: item.price,
+    // });
   }
   render() {
     const additional = this.props.additional
@@ -21,4 +36,18 @@ class CartItemAdditional extends Component {
   }
 }
 
-export default CartItemAdditional;
+const mapStateToProps = (state) => {
+  return {
+    items: state.items,
+    totalAmount: state.totalAmount,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addItem: (item) => dispatch(cartActions.addItem(item)),
+    removeItem: (id) => dispatch(cartActions.removeItem(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartItemAdditional);
